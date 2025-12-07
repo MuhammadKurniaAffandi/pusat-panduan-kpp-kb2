247f0e99ca03e4be14ab95b83bdaa6bfc9997be8
Muhammad Kurnia Affandi <affandi.kurnia@gmail.com>
2025-12-07 10:11:15 +0700

    ganti favicon dan sembungikan folder uploads di api

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,12 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/lib/query-provider";
 import { Toaster } from "@/components/ui/sonner";
-
+import Favicon from "./logo-kementerian-keuangan-356.png";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pusat Panduan - KPP Pratama Jakarta Kebayoran Baru Dua",
   description: "Pusat panduan dan informasi layanan perpajakan",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({
